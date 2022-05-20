c727fb0f133764dee8834a6cac2fe32eec7d9991
Ashley Peterson <axp701@pediatrics.wisc.edu>
2022-05-20 15:59:13 -0500

    Fixed the J issue

--- 2D_data_clean-up_ANP.R
+++ 2D_data_clean-up_ANP.R
@@ -6,18 +6,28 @@ library(tidyverse)
 
 ## READ IN DATA
 #DATA <- read_csv("C:/Users/anpet/Desktop/2D_exp_full_data/movies/C5a movies/Arpc1b lines/2D/Variables/2D LUT_formatted4R.csv")
-DATA <- read_csv("C:/Users/axp701/In-vitro-cell-motility-v2/2D LUT_formatted4R.csv")
+DATA <- read.csv("C:/Users/axp701/In-vitro-cell-motility-v2/2D LUT_formatted4R.csv")
 
 #data_results <- list.files(path = "data", full.names = T) 
 #DATA <- read.csv(data_results)
 #rm(data_results)
 
-# View a portion of DATA 
+# View a portion of DATA-keep this DATA clean, for posterity 
 head(DATA)
+wksp <- DATA
 
+# factor by cell line
+(a <- unique(wksp$cell_line)) # cline = cell line
+wksp$f.cell_line <- factor(wksp$cell_line, levels=a, labels=c("Control", "Arpc1b")) 
+rm(a)
+
+
+wksp <- subset(wksp, wksp$stimulus != "PBS")
+# factor data by stimulus
+wksp$f.stim <- factor(wksp$stimulus, levels=c("PBS","fMLF_1uM","Low_3uM_C5a","High_30uM_C5a"),labels=c("PBS","fMLF 1uM","C5a 3uM","C5a 30uM"))
 
 # create new data structure with na values excluded
-wksp <- DATA[!is.na(DATA$track_length), ]
+wksp <- wksp[!is.na(wksp$track_length), ]
 wksp <- wksp[!is.na(wksp$cumul_distance), ]
 wksp <- wksp[!is.na(wksp$net_displacement), ]
 wksp <- wksp[!is.na(wksp$tortuosity), ]
@@ -26,6 +36,9 @@ wksp <- wksp[!is.na(wksp$mean_theta), ]
 wksp <- wksp[!is.na(wksp$mean_CI), ]
 wksp <- wksp[!is.na(wksp$num_pauses), ]
 wksp <- wksp[!is.na(wksp$pause_duration), ]
+
+# ANP-keep the naming convention that f.day means the day column factored by day
+wksp$f.day <- as.factor(wksp$day)
 
 # Creating additional value, J and then aggregating data by summing over speed and J. Later, we will use the
 # value of summed J to divide for calculating mean speed
@@ -41,31 +54,27 @@ wksp$J <- 1
 # format: rename(new_name = old_name)
 
 
-  
+#data_clean = wksp %>%
+  #rename(cell_line = cline)  %>%
+  #rename(cell_speed = m.speed) %>% # give more intelligible names
+  #rename(directionality_index = m.di) %>%
+  #rename(cumulative_distance = distance) %>%
+  #rename(confinement_radius = mean_cr) %>%
+  #rename(stimulus = f.stim) %>%
+  #rename(diff_coeff = mean_dc) %>%
+  #select(cell_line, stimulus, f.day, slide, id, J, mean_velocity, net_displacement, 
+         #cumul_distance, mean_CI, tortuosity, confinement_radius, track_length)
 #select(wksp, cell_line, stimulus, f.day, slide, id, J, track_length, cumul_distance, net_displacement,tortuosity,mean_velocity,mean_theta, mean_CI, num_pauses,pause_duration, confinement_radius, confined_vel, free_vel,super_vel)
-
-# factor by cell line
-(a <- unique(wksp$cell_line)) # cline = cell line
-wksp$f.cell_line <- factor(wksp$cell_line, levels=a, labels=c("Control", "Arpc1b")) 
-rm(a)
-
-# ANP-keep the naming convention that f.day means the day column factored by day
-wksp$f.day <- as.factor(wksp$day)
-
-
-wksp <- subset(wksp, wksp$stimulus != "PBS")
-# factor data by stimulus
-wksp$f.stim <- factor(wksp$stimulus, levels=c("PBS","fMLF_1uM","Low_3uM_C5a","High_30uM_C5a"),labels=c("PBS","fMLF 1uM","C5a 3uM","C5a 30uM"))
 
 # Remove tracks of cells that didn't move. This should be already removed from Imaris settings
 wksp <- wksp[!is.na(wksp$num_pauses), ]
-#wksp <- wksp[!is.na(wksp$free_vel), ]
+wksp <- wksp[!is.na(wksp$free_vel), ]
 
 # ANP-I don't think this is needed anymore
 motility_clean = wksp %>%
   #rename(stimulus = f.stim) %>%
   #rename(vFree = free_vel) %>%
-  select(cell_line, stimulus, f.day, slide, id, J, num_pauses)
+  select(cell_line, stimulus, f.day, slide, id, J, num_pauses, free_vel)
 
 wksp <- wksp[!is.na(wksp$pause_duration), ]
 
@@ -76,16 +85,16 @@ tPause_clean = wksp %>%
   #rename(tPause = durPause) %>%
   select(cell_line, stimulus, f.day, slide, id, J, pause_duration)
   
-
-
 ## FILTER DATA ####
 # Next, I'm going to gather some summary statistics and use that to filter out outliers. Here, I will not be filtering 
 # out anything from the low end - as these may be immotile cells. I will filter out anything with physiologically 
 # irrelevant speed and distance measures as these are typically due to tracking errors. I will not filter anything that 
 # is already mathematically confined (ex. CI between -1 and 1)
 
+data_clean <- wksp
+
 # Get speed outlier information
-data_speed_sum = wksp %>%
+data_speed_sum = data_clean %>%
   group_by(cell_line, f.day, stimulus) %>%
   summarise(speed_mean = mean(mean_velocity),
             speed_sd = sd(mean_velocity)) %>%
@@ -93,9 +102,14 @@ data_speed_sum = wksp %>%
   mutate(speed_high = speed_mean + (2 * speed_sd)) 
 
 # Remove any data points with excessively high speeds on the assumption that they are due to tracking errors
-wksp = wksp %>%
+data_clean = data_clean %>%
   inner_join(data_speed_sum) %>%
   filter(mean_velocity < speed_high) 
-
 rm(data_speed_sum)
+
+agg <- data_clean
+
+agg <- aggregate( cbind(track_length,cumul_distance,net_displacement,tortuosity, mean_velocity, mean_theta, mean_CI, num_pauses, pause_duration, confinement_radius, confined_vel, free_vel, super_vel, J) ~ f.day + slide + cell_line + stimulus, data=agg, FUN=sum)
+agg$trt <- apply(agg[,c("stimulus","cell_line")], 1, paste, sep="", collapse=":")
+agg$trt <- as.factor(agg$trt)
 
